b4e5885c53afb8ff8fb96a9be46bc665616e673c
htc <htc550605125@gmail.com>
2016-04-27 22:24:29 +0800

    change permission

--- host/init-host.sh
+++ host/init-host.sh
@@ -2,12 +2,9 @@
 
 PREFIX=/usr/local/htcNet-scripts
 
-pushd
 cd /usr/local
 git clone git@github.com:htc550605125/htcNet-scripts.git
 
 cp "$PREFIX"/htcNet/htcNet.conf /etc/htcNet.conf
 
 "$PREFIX"/host/update-host.sh
-
-popd
