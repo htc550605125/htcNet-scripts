6351c773875e15748a890370c9e25a623d79ff7d
htc <htc550605125@gmail.com>
2016-04-28 23:19:15 +0800

    fix url

--- host/init-host.sh
+++ host/init-host.sh
@@ -3,7 +3,7 @@
 PREFIX=/usr/local/htcNet-scripts
 
 cd /usr/local
-git clone ssh://git@github.com:htc550605125/htcNet-scripts.git
+git clone git@github.com:htc550605125/htcNet-scripts.git
 
 cp "$PREFIX"/client/htcNet.conf /etc/htcNet.conf
 
